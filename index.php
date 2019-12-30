e06c37b86e75acd71da8d669ef67ef7020202b4f
Emiel <emielrietdijk@gmail.com>
2019-12-30 11:57:31 +0100

    Removed staged files from vendor. Updated gitignore. Updated php versions in composer Changed index to show only what we need. Added a check fo the telephone number in the signuature handling.

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Moometric\mooSignature;
 // Update with your GSuite domain and admin email address
-$admin_email = "admin@domain.com";
-$domain = "domain.com";
-$sigPath ="/your/project/path/signatures/";
-$serviceAccountPath = "/your/project/path/local_vars/";
+$admin_email = "edwin.dewit@ivido.nl";
+$domain = "ivido.nl";
+$sigPath = __DIR__ . "signatures/";
+$serviceAccountPath = __DIR__ . "/local_vars/";
 
 $mooSig = new mooSignature($domain, $admin_email);
 
@@ -15,57 +15,16 @@ $mooSig = new mooSignature($domain, $admin_email);
 //$mooSig->addsettingSignaturePath($sigPath);
 
 // Setting test and preview mode so no changes are written
+// For pushing to live: set addSettingRunTestMode to False
 $mooSig->addSettingRunTestMode(True);
 $mooSig->addSettingPreviewSignature(True);
 
 // Setting the default signature
-$mooSig->addSettingSetTemplate("defaultSig.html");
-
-echo "<h2>Updating a single user from domain</h2>";
-// Example 1: setting a single user from domain
+$mooSig->addSettingSetTemplate("ividoSignature.html");
+// All users from gsuite
 $mooSig->addSettingGetUsersFromGsuite(True);
+
+// For testing purposes. Comment to change all users
 $mooSig->addSettingFilterEmailsToUpdate(["$admin_email"]);
+
 $mooSig->updateSignatures();
-
-// Example 2: Set the MOTD to "Hello World" in red
-echo "<h2>Setting MOTD </h2>";
-$mooSig->addSettingMOTDHTML("<span style=\"color: red;\">Hello World</span>");
-$mooSig->addSettingMOTDPosition("Below");
-$mooSig->addSettingMOTD(True);
-$mooSig->setSignatureMOTD();
-
-echo "<h2>List of avaliable merge fields</h2>";
-// Example 3: For fun, list avaliable merge fields that can be used in your email template
-$mooSig->listMergeFields();
-
-echo "<h2>From array of users</h2>";
-// Example 4: setting users from array
-$mooSig->addSettingUnsetFilters();
-$mooSig->addSettingMOTDHTML("<span style=\"color: red;\">MOTD from Array/JSON</span>");
-$mooSig->addSettingUserArray([
-							[
-								"primaryEmail" => "fakeEmail@moometric.com", 
-								"alias" => "fakeEmail@moometric.com", 
-								"thumbnailPhotoUrl" => "https://i.imgur.com/JRF8XKq.png",
-								"fullName" => "MooMaster",
-								"phone0" => "555-555-555",
-								"title" => "IT Admin / Developer"
-							],
-							[
-								"primaryEmail" => "anotherEmail@moometric.com", 
-								"alias" => "anotherEmail@moometric.com", 
-								"thumbnailPhotoUrl" => "https://i.imgur.com/JRF8XKq.png",
-								"fullName" => "MooMinor",
-								"phone0" => "444-444-444",
-								"title" => "DevOps Admin"
-							]
-						]);
-$mooSig->updateSignatures();
-
-echo "<h2>From testUsers.JSON file</h2>";
-// Example 5: setting users from JSON file
-$mooSig->addSettingUsersFile("testUsers.json");
-$mooSig->updateSignatures();
-
-
-
